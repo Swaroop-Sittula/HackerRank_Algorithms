1507e309295328f20bc7f4c2beb0e4cf425e827e
Sittula Jyothi Swaroop <84241838+Swaroop-Sittula@users.noreply.github.com>
2021-06-30 13:04:17 +0530

    Create SubarrayDivision.cs

---
+++ SubarrayDivision.cs
@@ -0,0 +1,47 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int birthday(List<int> s, int d, int m)
+    {
+        //m - size of subarray
+        //d - sum value of 'm' consecutive values
+        var result = 0;
+        for(int i=0;i+m<=s.Count;i++)
+        {
+                //s.GetRange(i,m).Sum()
+            if(s.Skip(i).Take(m).Sum() == d)
+                result++;
+        }
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+        int d = Convert.ToInt32(firstMultipleInput[0]);
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+        int result = Result.birthday(s, d, m);
+        textWriter.WriteLine(result);
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
