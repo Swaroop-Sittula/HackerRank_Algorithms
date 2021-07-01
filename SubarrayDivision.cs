6e79911a65d7d8c2e73b79cf70721773de19cba0
Sittula Jyothi Swaroop <84241838+Swaroop-Sittula@users.noreply.github.com>
2021-07-01 11:35:21 +0530

    Update SubarrayDivision.cs

--- SubarrayDivision.cs
+++ SubarrayDivision.cs
@@ -11,7 +11,7 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
-
+/* https://www.hackerrank.com/challenges/the-birthday-bar/problem */
 class Result
 {
     public static int birthday(List<int> s, int d, int m)
