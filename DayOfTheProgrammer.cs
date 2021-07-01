5fd335deb3ed3b0f1bf5ae56366d2e05d778f620
Sittula Jyothi Swaroop <84241838+Swaroop-Sittula@users.noreply.github.com>
2021-07-01 11:33:33 +0530

    Update DayOfTheProgrammer.cs

--- DayOfTheProgrammer.cs
+++ DayOfTheProgrammer.cs
@@ -11,7 +11,7 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
-
+/* https://www.hackerrank.com/challenges/day-of-the-programmer/problem */
 class Result
 {
     public static string dayOfProgrammer(int year)
