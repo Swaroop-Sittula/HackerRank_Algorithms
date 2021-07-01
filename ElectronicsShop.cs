9c3eef274d7d32ddf74b8c246f5e8e7091d9c863
Sittula Jyothi Swaroop <84241838+Swaroop-Sittula@users.noreply.github.com>
2021-07-01 11:37:41 +0530

    Update ElectronicsShop.cs

--- ElectronicsShop.cs
+++ ElectronicsShop.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-
+/*  https://www.hackerrank.com/challenges/electronics-shop/problem */
 class Solution {
 
     static int getMoneySpent(int[] keyboards, int[] drives, int b) {
