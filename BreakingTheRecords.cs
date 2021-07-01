990da0785ecfebf1433d30f49e1583e4dbdd8833
Sittula Jyothi Swaroop <84241838+Swaroop-Sittula@users.noreply.github.com>
2021-07-01 11:32:01 +0530

    Update BreakingTheRecords.cs

--- BreakingTheRecords.cs
+++ BreakingTheRecords.cs
@@ -11,7 +11,7 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
-
+/* https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem */
 class Result
 {
     public static List<int> breakingRecords(List<int> scores)
