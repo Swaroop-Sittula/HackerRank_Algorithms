d2284d39bad50120e25c8404f2c4a72fabc72fc0
Sittula Jyothi Swaroop <84241838+Swaroop-Sittula@users.noreply.github.com>
2021-07-07 09:46:57 +0530

    Create CircularArrayRotation.cs

---
+++ CircularArrayRotation.cs
@@ -0,0 +1,58 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+/*  https://www.hackerrank.com/challenges/circular-array-rotation/problem   */
+class Result
+{
+    public static List<int> circularArrayRotation(List<int> a, int k, List<int> queries)
+    {
+        var result = new List<int>();
+        k = (a.Count - (k%a.Count))%a.Count; //takes care of case k = a.Count.
+        result = queries.Select(c => a[(k+c)%a.Count]).ToList();
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        int q = Convert.ToInt32(firstMultipleInput[2]);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> queries = new List<int>();
+
+        for (int i = 0; i < q; i++)
+        {
+            int queriesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            queries.Add(queriesItem);
+        }
+
+        List<int> result = Result.circularArrayRotation(a, k, queries);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
