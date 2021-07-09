7ea4ab606b57581b5dc10079af0e9fdf780a5d60
Sittula Jyothi Swaroop <84241838+Swaroop-Sittula@users.noreply.github.com>
2021-07-09 18:51:48 +0530

    Create EqualizeTheArray.cs

---
+++ EqualizeTheArray.cs
@@ -0,0 +1,43 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+/*  https://www.hackerrank.com/challenges/equality-in-a-array/problem   */
+class Result
+{
+
+    public static int equalizeArray(List<int> arr)
+    {
+        var maxCount = arr.GroupBy(c => c).OrderBy(c => c.Count()).Last().Count();
+        return arr.Count - maxCount;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.equalizeArray(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
