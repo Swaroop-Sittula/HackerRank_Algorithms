228ab3bb1d4e59c1d7f33404fd6358f80edb02ff
Sittula Jyothi Swaroop <84241838+Swaroop-Sittula@users.noreply.github.com>
2021-07-09 18:45:10 +0530

    Create CutTheSticks.cs

---
+++ CutTheSticks.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+/*  https://www.hackerrank.com/challenges/cut-the-sticks/problem   */
+class Result
+{
+    public static List<int> cutTheSticks(List<int> arr)
+    {
+        arr.Sort();
+        var result = new List<int>(){arr.Count};
+        while(arr.Count!=0)
+        {
+            arr = arr.Select(c => c-arr[0]).ToList();
+            arr.RemoveAll(c => c==0);
+            if(arr.Count!=0)
+                result.Add(arr.Count);
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.cutTheSticks(arr);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
