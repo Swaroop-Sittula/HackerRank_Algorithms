a6e871b2b29d6f6dd60785106029da8e1eeb112a
Sittula Jyothi Swaroop <84241838+Swaroop-Sittula@users.noreply.github.com>
2021-07-09 18:40:51 +0530

    Create SherlockAndSquares.cs

---
+++ SherlockAndSquares.cs
@@ -0,0 +1,49 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+/*  https://www.hackerrank.com/challenges/sherlock-and-squares/problem  */
+class Result
+{
+    
+    public static int squares(int a, int b)
+    {
+        return 1 + (int)Math.Floor(Math.Sqrt(b)) - (int)Math.Ceiling(Math.Sqrt(a));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int a = Convert.ToInt32(firstMultipleInput[0]);
+
+            int b = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = Result.squares(a, b);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
